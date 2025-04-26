640ef57a42a4beab58f99970d9b64cebd2008b4e
Yash V <72429905+yashvempati@users.noreply.github.com>
2025-04-27 00:55:17 +0530

    update chat_backend.py

--- backend/chat_backend.py
+++ backend/chat_backend.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import io
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import Ollama
 
